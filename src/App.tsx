ae4b06d0b2ffff7a3c0fa7cad2d7ced0d18913e9
leesojeong <sjlee8969@gmail.com>
2024-06-22 15:15:48 +0900

    Revert "Merge pull request #4 from Codeit-Sprint-6-Team4/feat-이소정"
    This reverts commit 712526c0840341d3091bd10de8c9b511e45b3b32, reversing
    changes made to bba4f4dacfe4a4d0ab4561973d835a44e8c714ed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import test from '@hooks/test';
-import GlobalStyle from './styles/global-styles';
 
 function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <h1>ddd</h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
