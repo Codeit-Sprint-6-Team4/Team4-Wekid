462f24f81fcbd48e16b524db933924f902fd3400
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-06-22 13:12:40 +0900

    feat: 컬러, 폰트 스타일 theme 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import test from '@hooks/test';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '@styles/theme';
 
 function App() {
-  return <></>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
