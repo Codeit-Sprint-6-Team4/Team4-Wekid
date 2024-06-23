8ac843bd193dcd24e36bfbd04dc7df0d338bfe26
chanho <lch0309k@gmail.com>
2024-06-23 19:38:21 +0900

    feat: snackber제작, theme수정

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,137 +4,149 @@ import { DefaultTheme } from 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
-      grayscale: {
+      gray: {
+        0: string;
+        50: string;
+        100: string;
+        300: string;
+        600: string;
+        800: string;
+        900: string;
+      };
+      green: {
         100: string;
         200: string;
         300: string;
-        400: string;
+      };
+      main: {
+        50: string;
         500: string;
         600: string;
       };
-      primaryGreen: {
-        100: string;
-        200: string;
-        300: string;
+      red: {
+        50: string;
+        500: string;
       };
-      secondaryRed: {
-        100: string;
-        200: string;
-      };
-      secondaryPurple: {
+      purple: {
         100: string;
       };
-      secondaryYellow: {
+      yellow: {
         100: string;
       };
     };
     fonts: {
-      'pretendard/5xl-48px-semibold': string;
+      '48px-600': string;
 
-      'pretendard/4xl-40px-bold': string;
+      '40px-700': string;
 
-      'pretendard/3xl-32px-bold': string;
-      'pretendard/3xl-32px-semibold': string;
+      '32px-700': string;
+      '32px-600': string;
 
-      'pretendard/2xl-24px-bold': string;
-      'pretendard/2xl-24px-semibold': string;
-      'pretendard/2xl-24px-medium': string;
-      'pretendard/2xl-24px-regular': string;
+      '24px-700': string;
+      '24px-600': string;
+      '24px-500': string;
+      '24px-400': string;
 
-      'pretendard/xl-20px-bold': string;
-      'pretendard/xl-20px-semibold': string;
-      'pretendard/xl-20px-medium': string;
-      'pretendard/xl-20px-regular': string;
+      '20px-700': string;
+      '20px-600': string;
+      '20px-500': string;
+      '20px-400': string;
 
-      'pretendard/2lg-18px-bold': string;
-      'pretendard/2lg-18px-semibold': string;
-      'pretendard/2lg-18px-medium': string;
-      'pretendard/2lg-18px-regular': string;
+      '18px-700': string;
+      '18px-600': string;
+      '18px-500': string;
+      '18px-400': string;
 
-      'pretendard/lg-16px-bold': string;
-      'pretendard/lg-16px-semibold': string;
-      'pretendard/lg-16px-medium': string;
-      'pretendard/lg-16px-regular': string;
+      '16px-700': string;
+      '16px-600': string;
+      '16px-500': string;
+      '16px-400': string;
 
-      'pretendard/md-14px-bold': string;
-      'pretendard/md-14px-semibold': string;
-      'pretendard/md-14px-medium': string;
-      'pretendard/md-14px-regular': string;
+      '14px-700': string;
+      '14px-600': string;
+      '14px-500': string;
+      '14px-400': string;
 
-      'pretendard/sm-13px-semibold': string;
-      'pretendard/sm-13px-medium': string;
+      '13px-600': string;
+      '13px-500': string;
 
-      'pretendard/xs-12px-semibold': string;
-      'pretendard/xs-12px-medium': string;
-      'pretendard/xs-12px-regular': string;
+      '12px-600': string;
+      '12px-500': string;
+      '12px-400': string;
     };
   }
 }
 
 export const theme: DefaultTheme = {
   colors: {
-    grayscale: {
-      100: '#F7F7FA',
-      200: '#E4E5F0',
+    gray: {
+      0: '#FFFFFF',
+      50: '#F7F7FA',
+      100: '#E4E5F0',
       300: '#C6CADA',
-      400: '#8F95B2',
-      500: '#474D66',
-      600: '#38415B',
+      600: '#8F95B2',
+      800: '#474D66',
+      900: '#38415B',
     },
-    primaryGreen: {
+    green: {
       100: '#EEF9F6',
       200: '#4CBFA4',
-      300: '#1B92FF',
+      300: '#32A68A',
     },
-    secondaryRed: {
-      100: '#FBEDED',
-      200: '#D14343',
+    main: {
+      50: '#EEF9F6',
+      500: '#4CBFA4',
+      600: '#32A68A',
     },
-    secondaryPurple: {
+    red: {
+      50: '#FBEDED',
+      500: '#D14343',
+    },
+    purple: {
       100: '#8E66FF',
     },
-    secondaryYellow: {
+    yellow: {
       100: '#FDD181',
     },
   },
   fonts: {
-    'pretendard/5xl-48px-semibold': 'semibold 48px/46px Pretendard',
+    '48px-600': 'normal normal 600 48px/46px Pretendard',
 
-    'pretendard/4xl-40px-bold': 'bold 40px/42px Pretendard',
+    '40px-700': 'normal normal 700 40px/42px Pretendard',
 
-    'pretendard/3xl-32px-bold': 'bold 32px/46px Pretendard',
-    'pretendard/3xl-32px-semibold': 'semibold 32px/42px Pretendard',
+    '32px-700': 'normal normal 700 32px/46px Pretendard',
+    '32px-600': 'normal normal 600 32px/42px Pretendard',
 
-    'pretendard/2xl-24px-bold': 'bold 24px/32px Pretendard',
-    'pretendard/2xl-24px-semibold': 'semibold 24px/32px Pretendard',
-    'pretendard/2xl-24px-medium': 'medium 24px/32px Pretendard',
-    'pretendard/2xl-24px-regular': 'regular 24px/32px Pretendard',
+    '24px-700': 'normal normal 700 24px/32px Pretendard',
+    '24px-600': 'normal normal 600 24px/32px Pretendard',
+    '24px-500': 'normal normal 500 24px/32px Pretendard',
+    '24px-400': 'normal normal 400 24px/32px Pretendard',
 
-    'pretendard/xl-20px-bold': 'bold 20px/32px Pretendard',
-    'pretendard/xl-20px-semibold': 'semibold 20px/32px Pretendard',
-    'pretendard/xl-20px-medium': 'medium 20px/32px Pretendard',
-    'pretendard/xl-20px-regular': 'regular 20px/32px Pretendard',
+    '20px-700': 'normal normal 700 20px/32px Pretendard',
+    '20px-600': 'normal normal 600 20px/32px Pretendard',
+    '20px-500': 'normal normal 500 20px/32px Pretendard',
+    '20px-400': 'normal normal 400 20px/32px Pretendard',
 
-    'pretendard/2lg-18px-bold': 'bold 18px/26px Pretendard',
-    'pretendard/2lg-18px-semibold': 'semibold 18px/26px Pretendard',
-    'pretendard/2lg-18px-medium': 'medium 18px/26px Pretendard',
-    'pretendard/2lg-18px-regular': 'regular 18px/26px Pretendard',
+    '18px-700': 'normal normal 700 18px/26px Pretendard',
+    '18px-600': 'normal normal 600 18px/26px Pretendard',
+    '18px-500': 'normal normal 500 18px/26px Pretendard',
+    '18px-400': 'normal normal 400 18px/26px Pretendard',
 
-    'pretendard/lg-16px-bold': 'bold 16px/26px Pretendard',
-    'pretendard/lg-16px-semibold': 'semibold 16px/26px Pretendard',
-    'pretendard/lg-16px-medium': 'medium 16px/26px Pretendard',
-    'pretendard/lg-16px-regular': 'regular 16px/26px Pretendard',
+    '16px-700': 'normal normal 700 16px/26px Pretendard',
+    '16px-600': 'normal normal 600 16px/26px Pretendard',
+    '16px-500': 'normal normal 500 16px/26px Pretendard',
+    '16px-400': 'normal normal 400 16px/26px Pretendard',
 
-    'pretendard/md-14px-bold': 'bold 14px/24px Pretendard',
-    'pretendard/md-14px-semibold': 'semibold 14px/24px Pretendard',
-    'pretendard/md-14px-medium': 'medium 14px/24px Pretendard',
-    'pretendard/md-14px-regular': 'regular 14px/24px Pretendard',
+    '14px-700': 'normal normal 700 14px/24px Pretendard',
+    '14px-600': 'normal normal 600 14px/24px Pretendard',
+    '14px-500': 'normal normal 500 14px/24px Pretendard',
+    '14px-400': 'normal normal 400 14px/24px Pretendard',
 
-    'pretendard/sm-13px-semibold': 'semibold 13px/22px Pretendard',
-    'pretendard/sm-13px-medium': 'medium 13px/22px Pretendard',
+    '13px-600': 'normal normal 600 13px/22px Pretendard',
+    '13px-500': 'normal normal 500 13px/22px Pretendard',
 
-    'pretendard/xs-12px-semibold': 'semibold 12px/20px Pretendard',
-    'pretendard/xs-12px-medium': 'medium 12px/20px Pretendard',
-    'pretendard/xs-12px-regular': 'regular 12px/20px Pretendard',
+    '12px-600': 'normal normal 600 12px/20px Pretendard',
+    '12px-500': 'normal normal 500 12px/20px Pretendard',
+    '12px-400': 'normal normal 400 12px/20px Pretendard',
   },
 };
