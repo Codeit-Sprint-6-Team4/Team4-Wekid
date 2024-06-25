77ba6abc85b7397bcf0507ad9ff396e2d625f5f9
leedongkyu <leedong0225@icloud.com>
2024-06-25 20:27:01 +0900

    custum props

--- src/components/pagination/paginationButton.ts
+++ src/components/pagination/paginationButton.ts
@@ -3,7 +3,7 @@ import { theme } from '@styles/theme';
 import { media } from '@utils/media';
 
 interface StyledPaginationButtonProps {
-  params?: string;
+  $params?: string;
 }
 
 export const StyledPaginationButton = styled.button<StyledPaginationButtonProps>`
@@ -12,8 +12,8 @@ export const StyledPaginationButton = styled.button<StyledPaginationButtonProps>
   border-radius: 10px;
   background-color: #ffffffff;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-  color: ${({ params, children }) =>
-    params === children ? theme.colors.main[500] : theme.colors.gray[600]};
+  color: ${({ $params, children }) =>
+    $params === children ? theme.colors.main[500] : theme.colors.gray[600]};
   font-family: ${theme.fonts['pretendard/2lg-18px-regular']};
   font-size: 18px;
   ${media('mobile')`
