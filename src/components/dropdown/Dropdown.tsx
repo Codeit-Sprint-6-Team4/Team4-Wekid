14735dffa3367295a2925624d725f97c9c78e77b
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-06-26 14:30:22 +0900

    refactor: 스타일frame.ts 파일 분리

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-import ArrowIcon from '@assets/icons/icon-arrow.svg';
-import { media } from '@utils/media';
+import {
+  StyledDropdownButton,
+  StyledDropdownContainer,
+  StyledDropdownItem,
+  StyledDropdownList,
+} from './DropdownFrame';
 
 interface OptionType {
   id: number;
@@ -12,85 +15,6 @@ interface DropdownProps {
   options: OptionType[];
   onSelect: (option: OptionType) => void;
 }
-
-const StyledDropdownContainer = styled.div`
-  position: relative;
-  width: 200px;
-`;
-
-const StyledDropdownButton = styled.button<{ $isOpen: boolean }>`
-  width: 400px;
-  height: 45px;
-  padding: 10.5px 20px;
-  background-color: ${(props) => props.theme.colors.gray[0]};
-  border: none;
-  border-radius: 10px;
-  font: ${(props) => props.theme.fonts['pretendard/md-14px-regular']};
-  color: ${(props) => props.theme.colors.gray[600]};
-  text-align: left;
-  cursor: pointer;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  &:focus {
-    outline: none;
-  }
-
-  ${({ $isOpen: isOpen, theme }) =>
-    isOpen &&
-    `
-    border: 1px solid ${theme.colors.main[500]};
-    `}
-
-  .icon {
-    transition: transform 0.3s;
-    transform: ${({ $isOpen: isOpen }) =>
-      isOpen ? 'rotate(180deg)' : 'rotate(0deg)'};
-    width: 22px;
-    height: 22px;
-    background-image: url(${ArrowIcon});
-    background-size: cover;
-  }
-
-  ${media('mobile')`
-    width: 335px;
-  `}
-`;
-
-const StyledDropdownList = styled.ul`
-  position: absolute;
-  width: 400px;
-  padding: 6px 4px;
-  margin-top: 5px;
-  border: none;
-  background-color: ${(props) => props.theme.colors.gray[0]};
-  border-radius: 10px;
-  z-index: 1000;
-
-  ${media('mobile')`
-    width: 335px;
-    padding: 6px 3.35px;
-  `}
-`;
-
-const StyledDropdownItem = styled.li`
-  width: 392px;
-  height: 35px;
-  padding-left: 16px;
-  padding-top: 5px;
-  font: ${(props) => props.theme.fonts['pretendard/md-14px-regular']};
-  color: ${(props) => props.theme.colors.gray[800]};
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${(props) => props.theme.colors.main[50]};
-  }
-
-  ${media('mobile')`
-    width: 328.3px;
-  `}
-`;
 
 const Dropdown: React.FC<DropdownProps> = ({ options, onSelect }) => {
   const [isOpen, setIsOpen] = useState(false);
