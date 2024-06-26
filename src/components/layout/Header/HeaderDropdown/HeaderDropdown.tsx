f84b43b10d4b5187f5698686cd0f6d99a4f5fe7e
leesojeong <sjlee8969@gmail.com>
2024-06-26 12:23:11 +0900

    feat: HeaderDropdown 컴포넌트 제작

---
+++ src/components/layout/Header/HeaderDropdown/HeaderDropdown.tsx
@@ -0,0 +1,68 @@
+import React, { ReactNode, RefObject } from 'react';
+import styled from 'styled-components';
+
+interface DropdownProps {
+  dropdownRef: RefObject<HTMLDivElement>;
+  isOpen: boolean;
+  handleToggleDropdown: () => void;
+  children: ReactNode;
+  icon: string;
+}
+
+const HeaderDropdown: React.FC<DropdownProps> = ({
+  dropdownRef,
+  isOpen,
+  handleToggleDropdown,
+  children,
+  icon,
+}) => (
+  <DropdownContainer ref={dropdownRef}>
+    <StyledDropdownButton onClick={handleToggleDropdown}>
+      <StyledIcon>
+        <img src={icon} alt="icon" />
+      </StyledIcon>
+    </StyledDropdownButton>
+    {isOpen && (
+      <StyledDropdownContentWrap>{children}</StyledDropdownContentWrap>
+    )}
+  </DropdownContainer>
+);
+
+const DropdownContainer = styled.div`
+  display: inline-block;
+  position: relative;
+`;
+
+const StyledDropdownButton = styled.button`
+  cursor: pointer;
+`;
+
+const StyledIcon = styled.div`
+  width: 3.2rem;
+  height: 3.2rem;
+  border-radius: 100%;
+
+  & img {
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const StyledDropdownContentWrap = styled.div`
+  display: block;
+  position: absolute;
+  right: calc(0% - 1rem);
+  top: calc(100% + 0.4rem);
+  z-index: 1;
+  min-width: 12rem;
+  max-width: 34rem;
+  border-radius: 1rem;
+  overflow: hidden;
+  background-color: #fff;
+  box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.08);
+`;
+
+export default HeaderDropdown;
+
