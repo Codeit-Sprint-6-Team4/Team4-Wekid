fd05949854b99eea04ec5494dcf11558846399a0
강은우 <80903685+eunwoo66@users.noreply.github.com>
2024-06-26 15:35:11 +0900

    refactor: 스타일 Frame.ts 파일 분리

---
+++ src/components/button/ButtonFrame.ts
@@ -0,0 +1,75 @@
+import styled, { css } from 'styled-components';
+import { media } from '@utils/media';
+import { ButtonProps } from '@components/button/Button';
+
+export const StyledButton = styled.button<ButtonProps>`
+  width: ${({ $width: width }) => width || '120px'};
+  height: ${({ $height: height }) => height || '40px'};
+  border-radius: 10px;
+  border: none;
+  font: ${(props) => props.theme.fonts['pretendard/lg-14px-semibold']};
+
+  ${(props) =>
+    props.$primary &&
+    css`
+      background-color: ${props.theme.colors.main[500]};
+      color: ${props.theme.colors.gray[0]};
+      &:disabled {
+        background-color: ${props.theme.colors.gray[300]};
+      }
+    `}
+
+  ${(props) =>
+    props.$secondary &&
+    css`
+      background-color: transparent;
+      color: ${props.theme.colors.main[500]};
+      border: 1px solid ${props.theme.colors.main[500]};
+      &:disabled {
+        color: ${props.theme.colors.gray[300]};
+        border-color: ${props.theme.colors.gray[300]};
+      }
+    `}
+
+  ${(props) =>
+    props.$loading &&
+    css`
+      background-color: ${props.theme.colors.gray[300]};
+      color: ${props.theme.colors.gray[0]};
+      cursor: not-allowed;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    `}
+    
+    ${(props) =>
+    props.$mobileWidth &&
+    media('mobile')`
+    width: ${props.$mobileWidth};
+  `}
+`;
+
+export const StyledDots = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 10px;
+
+  span {
+    width: 4px;
+    height: 4px;
+    background-color: ${(props) => props.theme.colors.gray[0]};
+    border-radius: 50%;
+    margin-right: 4px;
+
+    &:nth-child(1) {
+      opacity: 0.5;
+    }
+    &:nth-child(2) {
+      opacity: 0.75;
+    }
+    &:nth-child(3) {
+      opacity: 1;
+    }
+  }
+`;
