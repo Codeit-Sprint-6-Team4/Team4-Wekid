28b529fb905c21b39e988f631397d85ebf595fc3
leesojeong <sjlee8969@gmail.com>
2024-06-28 05:02:35 +0900

    add: 시맨틱을 위한 blind 클래스 추가

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -158,6 +158,17 @@ const GlobalStyle = createGlobalStyle`
   input::-webkit-outer-spin-button {
     -webkit-appearance: none;
   }
+
+  .blind {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    padding: 0;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    border: 0;
+  }
 `;
 
 export default GlobalStyle;
