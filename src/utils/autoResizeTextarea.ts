48aced39f2c9c234def935717c72000d5836f9ce
leesojeong <sjlee8969@gmail.com>
2024-06-29 12:11:07 +0900

    refactor

--- src/utils/autoResizeTextarea.ts
+++ src/utils/autoResizeTextarea.ts
@@ -3,7 +3,7 @@
  * @param {HTMLAreaElement} textarea 
  */
 
-export const autoResizeTextarea = (textarea: HTMLAreaElement) => {
+export const autoResizeTextarea = (textarea: HTMLTextAreaElement) => {
   textarea.style.height = 'auto';
   textarea.style.height = textarea.scrollHeight + 'px';
 };
