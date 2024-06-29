36442f672482f57d0237613723cd5adcaaf0dd24
chanho <lch0309k@gmail.com>
2024-06-29 17:35:14 +0900

    feat: 리프래쉬토큰으로 엑세스토큰 갱신 기능 추가

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,6 +1,5 @@
-import axios, { AxiosError } from 'axios';
+import { AxiosError } from 'axios';
 import Cookies from 'js-cookie';
-import { logInDataTypes } from '@pages/SignIn/components/SignInContainer';
 import { userDataTypes } from '@pages/SignUp/components/SignUpContainer';
 import instance from './axios';
 
@@ -20,20 +19,21 @@ export const postSignUp = async (userData: userDataTypes) => {
   }
 };
 
-export const postSignIn = async (userData: logInDataTypes) => {
-  const URL = '/auth/signIn';
+export const refreshAccessToken = async () => {
+  const URL = '/auth/refresh';
+  const refreshToken = Cookies.get('refreshToken');
+  if (!refreshToken) {
+    throw new Error('리프레쉬토큰을 사용할 수 없음');
+  }
   try {
-    const response = await instance.post(URL, userData);
-    console.log(response);
+    const response = await instance.post(URL, { refreshToken: refreshToken });
     Cookies.set('accessToken', response.data.accessToken);
-    Cookies.set('refreshToken', response.data.refreshToken);
-    // Cookies,set('accessToken', response)
+    return response.data.accessToken;
   } catch (error) {
-    const err = error as AxiosError;
-    if (err.response) {
-      console.error('Response error:', err.response.status);
-      console.error('Response data:', err.response.data);
-      throw err;
+    if (error instanceof AxiosError) {
+      console.error('Response error:', error.response?.status);
+      console.error('Response data:', error.response?.data);
+      throw error;
     }
     console.error(error);
     throw error;
