9d67cf0f6f110c530a54ff972bfe7d515836a734
thatsrightcat <149943838+thatsrightcat@users.noreply.github.com>
2024-06-29 12:13:21 +0900

    Revert "feat: textarea의 높이를 자동으로 조절하는 유틸함수"

--- src/utils/autoResizeTextarea.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @description ex: autoResizeTextarea(textarea) dom에서 사용해야함
- * @param {HTMLAreaElement} textarea 
- */
-
-export const autoResizeTextarea = (textarea: HTMLAreaElement) => {
-  textarea.style.height = 'auto';
-  textarea.style.height = textarea.scrollHeight + 'px';
-};
