f17259da4a876eb51749a79ae319dc6678921449
chanho <lch0309k@gmail.com>
2024-06-29 19:16:28 +0900

    feat: 앤드포인트 변경

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,5 +1,6 @@
 import { AxiosError } from 'axios';
 import Cookies from 'js-cookie';
+import { logInDataTypes } from '@pages/SignIn/components/SignInContainer';
 import { userDataTypes } from '@pages/SignUp/components/SignUpContainer';
 import instance from './axios';
 
@@ -19,8 +20,28 @@ export const postSignUp = async (userData: userDataTypes) => {
   }
 };
 
+export const postSignIn = async (userData: logInDataTypes) => {
+  const URL = '/auth/signIn';
+  try {
+    const response = await instance.post(URL, userData);
+    console.log(response);
+    Cookies.set('accessToken', response.data.accessToken);
+    Cookies.set('refreshToken', response.data.refreshToken);
+    // Cookies,set('accessToken', response)
+  } catch (error) {
+    const err = error as AxiosError;
+    if (err.response) {
+      console.error('Response error:', err.response.status);
+      console.error('Response data:', err.response.data);
+      throw err;
+    }
+    console.error(error);
+    throw error;
+  }
+};
+
 export const refreshAccessToken = async () => {
-  const URL = '/auth/refresh';
+  const URL = '/auth/refresh-token';
   const refreshToken = Cookies.get('refreshToken');
   if (!refreshToken) {
     throw new Error('리프레쉬토큰을 사용할 수 없음');
