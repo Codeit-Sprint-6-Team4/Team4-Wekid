a74a46dafba4d1a2e531116b961f38b8f801a238
thatsrightcat <149943838+thatsrightcat@users.noreply.github.com>
2024-06-29 11:51:54 +0900

    refactor:  타입, 설명 추가

--- src/utils/autoResizeTextarea.ts
+++ src/utils/autoResizeTextarea.ts
@@ -1,4 +1,9 @@
-export const autoResizeTextarea = (textarea) => {
+/**
+ * @description ex: autoResizeTextarea(textarea) dom에서 사용해야함
+ * @param {HTMLAreaElement} textarea 
+ */
+
+export const autoResizeTextarea = (textarea: HTMLAreaElement) => {
   textarea.style.height = 'auto';
   textarea.style.height = textarea.scrollHeight + 'px';
 };
