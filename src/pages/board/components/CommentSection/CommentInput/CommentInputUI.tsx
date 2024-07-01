9a5058b11c47b141b2032a6aa88c22bab18a08c7
leesojeong <sjlee8969@gmail.com>
2024-07-01 17:26:48 +0900

    feat: 댓글 100자 제한

--- src/pages/board/components/CommentSection/CommentInput/CommentInputUI.tsx
+++ src/pages/board/components/CommentSection/CommentInput/CommentInputUI.tsx
@@ -10,17 +10,19 @@ interface CommentInputUIProps {
 }
 
 const CommentInputUI: React.FC<CommentInputUIProps> = ({ text, setText, handleSubmit, textareaRef }) => {
+  const isDisabled = text.length === 0 || text.length > 100;
+
   return (
     <CommentInputWrapper>
       <StyledCommentInput
         ref={textareaRef}
         value={text}
-        onChange={e => setText(e.target.value)}
+        onChange={e => setText(e.target.value.slice(0, 100))} // 100자 제한
         placeholder="댓글을 입력해주세요"
-      ></StyledCommentInput>
+      />
       <CommentInfo>
         <CharacterCount>{text.length}/100</CharacterCount>
-        <Button $primary onClick={handleSubmit}>댓글등록</Button>
+        <Button $primary onClick={handleSubmit} disabled={isDisabled}>댓글등록</Button>
       </CommentInfo>
     </CommentInputWrapper>
   );
