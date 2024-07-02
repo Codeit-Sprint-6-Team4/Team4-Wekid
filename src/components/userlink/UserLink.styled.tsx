7a1a36ddfbebf4744696465e9f8b37e36640fd12
chanho <lch0309k@gmail.com>
2024-07-02 20:11:07 +0900

    feat: 링크 클릭시 복사기능

--- src/components/userlink/UserLink.styled.tsx
+++ src/components/userlink/UserLink.styled.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
 import styled from 'styled-components';
 import linkIcon from '@assets/icons/icon-link-green.svg';
 
-interface UserLinkProps {
-  code: string;
-}
-
-const UserLinkStyled = styled.a`
+export const UserLinkStyled = styled.div`
   display: flex;
+  max-width: 400px;
   align-items: center;
   padding: 3px 10px;
   background-color: ${({ theme }) => theme.colors.main[50]};
   border-radius: 10px;
   color: ${({ theme }) => theme.colors.main[500]};
   font: ${({ theme }) => theme.fonts['pretendard/md-14px-regular']};
-  white-space: nowrap;
+
+  span {
+    flex: 1;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
 
   &::before {
     content: '';
+    flex-shrink: 0;
     display: inline-block;
     width: 18px;
     height: 18px;
@@ -28,11 +31,8 @@ const UserLinkStyled = styled.a`
     background-image: url(${linkIcon});
   }
 
-  &:hover {
-    text-decoration: underline;
-  }
-
   @media (max-width: 480px) {
+    max-width: 202px;
     padding: 4px 10px;
     font: ${({ theme }) => theme.fonts['pretendard/xs-12px-regular']};
 
@@ -43,12 +43,3 @@ const UserLinkStyled = styled.a`
     }
   }
 `;
-
-const baseURL = 'https://www.widied.kr/';
-
-const UserLinkContainer: React.FC<UserLinkProps> = ({ code }) => {
-  const url = `${baseURL}${code}`;
-  return <UserLinkStyled href={url}>{url}</UserLinkStyled>;
-};
-
-export default UserLinkContainer;
--- src/components/userlink/UserLink.tsx
+++ src/components/userlink/UserLink.tsx
@@ -1,12 +1,66 @@
-import React from 'react';
-import UserLinkContainer from './UserLinkContainer';
+import React, { useEffect, useState } from 'react';
+import Snackbar from '@components/snackbar/Snackbar';
+import { UserLinkStyled } from './UserLink.styled';
 
 interface UserLinkProps {
   code: string;
+  type?: 'info' | 'success' | 'error';
+  visible: boolean;
+  position?: string;
+  top?: string;
+  bottom?: string;
+  left?: string;
+  right?: string;
 }
+const baseURL = 'https://www.widied.kr/wiki/';
 
-const UserLink: React.FC<UserLinkProps> = ({ code }) => {
-  return <UserLinkContainer code={code} />;
+const UserLink: React.FC<UserLinkProps> = ({
+  code,
+  type = 'success',
+  visible,
+  position,
+  top,
+  bottom,
+  left,
+  right,
+}) => {
+  const [copied, setCopied] = useState(false);
+  const url = `${baseURL}${code}`;
+
+  const handleLinkClick = () => {
+    navigator.clipboard
+      .writeText(url)
+      .then(() => {
+        setCopied(true);
+      })
+      .catch((err) => {
+        console.error('주소 복사 실패', err);
+      });
+  };
+
+  useEffect(() => {
+    if (copied) {
+      const timer = setTimeout(() => setCopied(false), 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [copied]);
+
+  return (
+    <UserLinkStyled onClick={handleLinkClick}>
+      {url}
+      {copied && (
+        <Snackbar
+          type={type}
+          visible={visible}
+          position={position}
+          top={top}
+          bottom={bottom}
+          left={left}
+          right={right}
+        />
+      )}
+    </UserLinkStyled>
+  );
 };
 
 export default UserLink;
