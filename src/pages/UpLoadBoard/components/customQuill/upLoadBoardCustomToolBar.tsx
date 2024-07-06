b71f1ed6946e76b8afc55e2ca56e4ce4f391449a
chanho <lch0309k@gmail.com>
2024-07-06 09:52:31 +0900

    feat: 글자색상추가, 버튼 마우스 오버 시 설명

--- src/pages/UpLoadBoard/components/customQuill/upLoadBoardCustomToolBar.tsx
+++ src/pages/UpLoadBoard/components/customQuill/upLoadBoardCustomToolBar.tsx
@@ -3,29 +3,37 @@ const CustomBoardToolBar = () => {
     <div id="toolbar">
       <div className="frame-ql-fromats">
         <span className="ql-formats">
-          <button className="ql-bold" />
-          <button className="ql-italic" />
-          <button className="ql-underline" />
+          <button className="ql-bold" title="굵게" />
+          <button className="ql-italic" title="기울임꼴" />
+          <button className="ql-underline" title="밑줄" />
         </span>
 
         <span className="ql-formats">
-          <button className="ql-align" value="" />
-          <button className="ql-align" value="center" />
-          <button className="ql-align" value="right" />
+          <button className="ql-align" value="" title="왼쪽 정렬" />
+          <button className="ql-align" value="center" title="가운데 정렬" />
+          <button className="ql-align" value="right" title="오른쪽 정렬" />
         </span>
 
         <span className="ql-formats">
-          <button className="ql-list" value="bullet"></button>
-          <button className="ql-list" value="ordered"></button>
+          <button
+            className="ql-list"
+            value="bullet"
+            title="글머리 기호 목록"
+          ></button>
+          <button
+            className="ql-list"
+            value="ordered"
+            title="번호 매기기 목록"
+          ></button>
         </span>
 
         <span className="ql-formats">
-          <button className="ql-color" />
-          <button className="ql-image" />
+          <select className="ql-color" title="글자 색상"></select>
+          <button className="ql-image" title="이미지 삽입" />
         </span>
 
         <span className="ql-formats ql-toolbar-right">
-          <button className="ql-link" />
+          <button className="ql-link" title="링크 삽입" />
         </span>
       </div>
     </div>
