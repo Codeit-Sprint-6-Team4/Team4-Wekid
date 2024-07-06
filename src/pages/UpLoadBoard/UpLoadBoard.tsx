60a00b4777da86f68e3b79b79bf182ad00afc6f1
chanho <lch0309k@gmail.com>
2024-07-06 10:57:13 +0900

    style: helmet 내용수정

--- src/pages/UpLoadBoard/UpLoadBoard.tsx
+++ src/pages/UpLoadBoard/UpLoadBoard.tsx
@@ -5,10 +5,7 @@ import UploadBoardContainer from './components/UpLoadBoardContainer';
 const UpLoadBoard = () => {
   <Helmet>
     <title>Wikied - 자유게시글</title>
-    <meta
-      name="description"
-      content="위키드에 회원가입하고 다양한 서비스를 이용해 보세요. 회원가입 바로가기"
-    />
+    <meta name="description" content="위키드에 자유롭게 글을 적어보세요." />
     <meta name="keywords" content="위키드, 나만의 위키, 위키 커뮤니티" />
   </Helmet>;
   return <UploadBoardContainer />;
