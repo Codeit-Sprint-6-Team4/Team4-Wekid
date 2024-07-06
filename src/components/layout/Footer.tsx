2f500e4a4cbe9e8915da2d922ea9578b77947ab6
yumi-kim-0827 <116349476+yumi-kim-0827@users.noreply.github.com>
2024-07-06 16:51:47 +0900

    fix:반응형 footer 폰트 사이즈

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -30,6 +30,9 @@ const StyledFooter = styled.footer`
     flex-wrap: wrap;
     li {
       font: ${theme.fonts['pretendard/lg-14px-semibold']};
+      ${media('mobile')`
+      font: ${theme.fonts['pretendard/xs-12px-regular']};
+    `}
     }
     &:last-child {
       gap: 20px;
@@ -39,6 +42,9 @@ const StyledFooter = styled.footer`
   padding: 20px;
 line-height:2;
   `}
+  ${media('mobile')`
+      font: ${theme.fonts['pretendard/xs-12px-regular']};
+    `}
 `;
 
 const StyledUl = styled.ul`
@@ -57,9 +63,6 @@ const StyledUl = styled.ul`
     ${media('tablet')`
     padding:0;
     font:${theme.fonts['pretendard/xs-12px-medium']};
-  `}
-    ${media('mobile')`
-    font-size:10px;
   `}
   }
 `;
